f2ae14b05f31a15269c0704b557cb1b1bd83ab3b
Justine <>
2018-02-15 14:33:34 +0100

    directors added

--- app/services/search_movie.rb
+++ app/services/search_movie.rb
@@ -9,6 +9,10 @@ class SearchMovie
 	end
 
 	def find_movies(movie)
-		Tmdb::Search.movie(movie).results
+		Tmdb::Search.movie(movie).results.each do |movie|
+			director = Tmdb::Movie.director(movie.id).each do |director|
+				movie[:director] = director.name
+			end
+		end
 	end
 end
